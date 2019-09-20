40ea27a03a1a35110912c12d643ef977c66d1981
ziyueliang <liangziyue0306@126.com>
2019-09-19 22:11:15 -0400

    tooltip

---
+++ js/slider.js
@@ -0,0 +1,49 @@
+var slideIndex=1;
+showSlides(slideIndex);
+
+function plusSlides(n){
+  showSlides(slideIndex+=n);
+}
+
+function showSlides(n){
+  var i;
+  var slides=document.getElementsByClassName("mySlides");
+
+  // never-end loop
+  if(n>slides.length){
+    slideIndex=1;
+  }
+  if(n<1){
+    slideIndex=slides.length;
+  }
+
+
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display="none";
+  }
+
+  slides[slideIndex-1].style.display="block";
+
+}
+
+var divs = $('div[id^="text-"]').hide(),
+    i = 0;
+
+(function cycle() {
+
+    divs.eq(i).fadeIn(400)
+              .delay(1000)
+              .fadeOut(400, cycle);
+
+    i = ++i % divs.length;
+
+})();
+
+$('.tooltip').tooltipster({
+    theme: 'tooltipster-shadow'
+});
+
+
+$(document).ready(function() {
+            $('.tooltip').tooltipster();
+});
